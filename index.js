f900ebb9506e74d1e035369ef6856eaed319b8b6
Mingzhao91 <edwardwhite7@hotmail.com>
2023-02-24 19:53:30 +0000

    generate questions to get answers from user

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { LICENSES } = require("./utils/constants");
 
 // array of questions for user
-const questions = [
+let questions = [];
 
-];
+// generate a question by providing essential information
+function createQuestion(name, message, type = "input", choices) {
+  const question = {};
+
+  if (name) question.name = name;
+  if (message) question.message = message;
+  if (type) question.type = type;
+  if (choices) question.choices = choices;
+
+  return question;
+}
+
+// generate questions to put into inquirer
+function generateQuertions() {
+  const questionsArr = [];
+
+  questionsArr.push(
+    createQuestion("title", "Please enter the title for your project?")
+  );
+  questionsArr.push(
+    createQuestion(
+      "description",
+      "Please enter the description for your project?"
+    )
+  );
+  questionsArr.push(
+    createQuestion(
+      "installation",
+      "Please enter the installation instructions for your project?"
+    )
+  );
+  questionsArr.push(
+    createQuestion(
+      "usage",
+      "Please enter the usage information for your project?"
+    )
+  );
+  questionsArr.push(
+    createQuestion(
+      "contrubuting",
+      "Please enter the contribution guidelines for your project?"
+    )
+  );
+  questionsArr.push(
+    createQuestion(
+      "tests",
+      "Please enter the test instructions for your project?"
+    )
+  );
+  questionsArr.push(
+    createQuestion(
+      "license",
+      "Please choose the license for your project?",
+      "list",
+      LICENSES
+    )
+  );
+  questionsArr.push(
+    createQuestion("username", "Please enter your GitHub username?")
+  );
+  questionsArr.push(
+    createQuestion("email", "Please enter your email address?")
+  );
+
+  return questionsArr;
+}
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  questions = [...generateQuertions()];
 }
 
 // function call to initialize program
